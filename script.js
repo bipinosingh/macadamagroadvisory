ae9ef0ccdcf95a9b6eae6c80d15f8c62d4415fac
bipinomprakashsingh <bipinosingh@gmail.com>
2026-07-27 13:10:39 -0400

    Initial Macadam Agroadvisory website

---
+++ script.js
@@ -0,0 +1,22 @@
+document.querySelectorAll('a[href^="#"]').forEach(link=>{
+
+    link.addEventListener("click",function(e){
+    
+    let target=document.querySelector(this.getAttribute("href"));
+    
+    if(target){
+    
+    e.preventDefault();
+    
+    target.scrollIntoView({
+    
+    behavior:"smooth"
+    
+    });
+    
+    }
+    
+    });
+    
+    
+    });
